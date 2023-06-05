af1a8765eeac36a1130b50ccf72ca3b9e71b9cbf
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-06-05 10:53:38 +0000

    commit65

--- main.cpp
+++ main.cpp
@@ -1,12 +1,18 @@
 #include "mbed.h"
-
-DigitalOut myled(LED1);
-
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+#include "HEPTA_EPS.h"
+#include "HEPTA_SENSOR.h"
+HEPTA_EPS eps(PA_0,PA_4);
+HEPTA_SENSOR sensor(PA_7,PB_7,PB_6,0xD0);
+Serial pc(USBTX,USBRX,9600);
+int main()
+{   
+    float gx,gy,gz;
+    pc.printf("Angular Velocity Sensor\r\n");
+    eps.turn_on_regulator(); //Turn on 3.3V converter
+    sensor.setup();
+    for(int i=0;i<50;i++){
+        sensor.sen_gyro(&gx,&gy,&gz);
+        pc.printf("%f,%f,%f\r\n",gx,gy,gz);
+        wait_ms(1000);
+    }
 }
