196c3eafc3c357e249a863bb7464b4e8d704ee5b
Charlie.com <csru22005@g.nihon-u.ac.jp>
2024-12-08 09:48:02 +0000

    fixed main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include "mbed.h"
-#include "HEPTA_EPS.h"
-#include "HEPTA_SENSOR.h"
-HEPTA_EPS eps(PA_0,PA_4);
-HEPTA_SENSOR sensor(PA_7,PB_7,PB_6,0xD0);
+#include "LITE_EPS.h"
+#include "LITE_SENSOR.h"
+LITE_EPS eps(PA_0,PA_4);
+LITE_SENSOR sensor(PA_7,PB_7,PB_6);
 RawSerial pc(USBTX,USBRX,9600);
 int main()
 {   
     float gx,gy,gz;
-    pc.printf("Angular Velocity Sensor\r\n");
     eps.turn_on_regulator(); //Turn on 3.3V converter
-    sensor.setup();
+    sensor.set_up();
     for(int i=0;i<50;i++){
         sensor.sen_gyro(&gx,&gy,&gz);
         pc.printf("%f,%f,%f\r\n",gx,gy,gz);
